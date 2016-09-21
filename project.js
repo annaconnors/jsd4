b3e975c42409fab6bac71f6f69d7e22e118933fa
annaconnors <anna.m.connors@gmail.com>
2016-09-21 15:43:08 -0700

    adding class #2 and HW #1 files

---
+++ project.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+
+// Variable declaration 
+var color; 
+
+// Variable assignment 
+color = "red"
+
+// Declaration and Assignment at the same time
+var age = 21;
+
+// Declaration and not Assigned
+var started; 
