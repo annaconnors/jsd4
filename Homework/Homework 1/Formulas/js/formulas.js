e7e5e483076bec957b34ecb08a6109ea321d0a23
annaconnors <anna.m.connors@gmail.com>
2016-09-21 16:33:06 -0700

    formulas added

--- Homework/Homework 1/Formulas/js/formulas.js
+++ Homework/Homework 1/Formulas/js/formulas.js
@@ -6,3 +6,35 @@
 // 3.	 Be sure to link up this file in your HTML doc
 /////////////////////////////////////////////////////////////////////
 
+function calcFahrenheitToCelcius (x) {
+	var fahrenheitCalc = (x-32) * (5/9)
+	console.log(x)
+	console.log(fahrenheitCalc)
+	return fahrenheitCalc
+
+}
+
+function calcCelciusToFarenheit (y) {
+	var celciusCalc = y * (9/5) + 32
+	console.log(y)
+	console.log(celciusCalc)
+	return celciusCalc
+
+}
+
+function calcCircumference (r) {
+	var calcCirc = 2 * Math.PI * r
+	console.log(r)
+	console.log(calcCirc)
+	return calcCirc 
+
+}
+
+function calcLongestSide (a,b) {
+	var pythCalc = math.sqrt((a *a) + (b * b))
+	console.log(a)
+	console.log(b)
+	console.log(pythCalc)
+	return pythCalc
+
+}
