6c0280c277c7c042b7e10a2bd0640dafdcffdbad
annaconnors <anna.m.connors@gmail.com>
2016-11-07 17:27:57 -0800

    movie hw

--- Homework/homework6/movie-search/js/movieSearch.js
+++ Homework/homework6/movie-search/js/movieSearch.js
@@ -1,9 +1,10 @@
 // Setup
 // ----------------------------------------------
 
-var form = document.querySelector("form");
+var button = document.querySelector("button");
 
-var search = document.querySelector(".search");
+var title = document.querySelector("input");
+button.addEventListener("click", submit);
 
 var details = document.querySelector(".details");
 
@@ -18,10 +19,6 @@ var results = document.querySelector(".results");
 // ----------------------------------------------
 
 
-form.addEventListener('submit',getResults);
-
-results.addEventListener('click', detailsShown);
-
 
 // document.getElementById("results").addEventListener("click",function(e) {
 // 	if(e.target && e.target.nodeName == "LI") {
@@ -33,36 +30,77 @@ results.addEventListener('click', detailsShown);
 // ----------------------------------------------
 
 
-function detailsShown(event) {
-	var li = event.target.closest("li");
-	console.log(li); 
-	console.log(event.target); 
+function submit(){
+	event.preventDefault();
+	var query = "https://www.omdbapi.com/?s=" + title.value;
+	$.getJSON(query, displayMovie);
 }
 
+function displayMovie(json){
+	results.innerHTML = "";
+	getDetails(json.Search.shift().imdbID);
+        
+    json.Search.forEach(function(movie){
 
-function getResults(event) {
-	event.preventDefault(); 
+			var li = document.createElement("li");
+			var img = document.createElement("img");
+			var p = document.createElement("p");
 
-	var searchMovies = movies.value;
-	console.log(searchMovies);
-
-	var url = "http://www.omdbapi.com/?s=" + searchMovies; 
-
-	$.getJSON(url, showResults);
+		li.id = movie.imdbID;
+		img.src = movie.Poster;
+		img.id = movie.imdbID;
+		p.textContent = movie.Title;
+		p.id = movie.imdbID;	
+		results.appendChild(li);
+		li.appendChild(img);
+		li.appendChild(p);
+	})
 }
 
-function showResults(id) {
-	console.log('showResults', id); 
+function getDetails(id){
+	var query = "https://www.omdbapi.com/?i=" + id + "&plot=short";
+	$.getJSON(query, displayDetails);
+}
 
-	var display = id.value; 
-	console.log(display); 
+function displayDetails(movie){
+	details.innerHTML = "";
+	console.log(movie);
+	var div1 = document.createElement("div");
+	var img = document.createElement("img");
+	var div2 = document.createElement("div");
+	var h2 = document.createElement("h2");
+	var p1 = document.createElement("p");
+	var p2 = document.createElement("p");
+	var a = document.createElement("a");
+	var linkText = document.createTextNode("View on IMDb");
 
-	var query = "http://www.omdbapi.com/?i=" + id; 
+	div1.class = "image";
+	img.class = "poster";
+	img.src = movie.Poster;
+	div1.appendChild(img);
 
-	$.getJSON(query, showResults); 
+	div2.class = "text";
+	h2.class = "title";
+	h2.textContent = movie.Title;
+	p1.class = "plot";
+	p1.textContent = movie.Plot;
+	a.class = "imdb-link";
+	a.href = "http://www.imdb.com/title/"+movie.imdbID;
+
+ 	details.appendChild(div1);
+ 	div1.appendChild(img);
+ 	details.appendChild(div2);
+	div2.appendChild(h2);
+	div2.appendChild(p1);
+	div2.appendChild(p2);
+	p2.appendChild(a);
+	a.appendChild(linkText);
 
 }
 
+results.addEventListener("click", function(e) {
+	getDetails(e.target.id)
+});
 
 
 
